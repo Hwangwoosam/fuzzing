f9a63ece63d07ce45478ae5664744385ce247f45
Hwangwoosam <sam1783@naver.com>
2021-08-06 09:31:18 +0900

    8

---
+++ CH2/src/check_err.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "../include/fuzzer.h"
+
+
+void crash_if_too_long(char* s){
+    char buffer[] = "Thursday";
+    if(strlen(s) > strlen(buffer)){
+        perror("Too Long\n");
+    }
+}
+
+int hang_if_no_space(char* s){
+    int i = 0;
+    while(i < strlen(s)){
+        if(s[i] == ' '){
+            return 1;
+        }
+        i++;
+    }
+    return 0;
+}
+
+void collapse_if_too_large(char*s){
+    int s_num =atoi(s);
+    if(s_num > 1000){
+        perror("Too Large number\n");
+    }
+}
