9cd914054261a204eceb7cfceca86fe31be84407
Hwangwoosam <sam1783@naver.com>
2021-08-09 10:51:19 +0900

    leak information

--- CH2/src/2_info_leak.c
+++ CH2/src/2_info_leak.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/fuzzer.h"
+#include "../include/0_fuzzer.h"
 
 char * heartbeat(char* reply,int length,char* memory){
     for(int i = 0; i < strlen(reply); i++){
