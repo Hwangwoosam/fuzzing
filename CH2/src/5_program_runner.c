94d26658ea4083df69224177868d377faa3cbbb2
Hwangwoosam <sam1783@naver.com>
2021-08-10 13:21:30 +0900

    program_runner

--- CH2/src/5_program_runner.c
+++ CH2/src/5_program_runner.c
@@ -8,9 +8,9 @@
 #define FAIL "FAIL"
 #define UNRESOLVED "UNRESOLVED"
 
-int pipes[2];
-int pipes2[2];
-int pipes3[2];
+int pipe_stdin[2];
+int pipe_stdout[2];
+int pipe_stderr[2];
 
 typedef struct result{
     char* output ;
@@ -20,17 +20,16 @@ typedef struct result{
 }result_t;
 
 result_t* subprocess(char* program,char* str){
-    result_t* temp = (result_t*)malloc(sizeof(result_t));
-
-    if(pipe(pipes) != 0) {
+    int length = strlen(str);
+    if(pipe(pipe_stdin) != 0) {
         perror("pipe") ;
         exit(1) ;
     }
-    if (pipe(pipes2) != 0) {
+    if (pipe(pipe_stdout) != 0) {
         perror("pipe2") ;
         exit(1) ;
     }
-     if (pipe(pipes3) != 0) {
+     if (pipe(pipe_stderr) != 0) {
         perror("pipe3") ;
         exit(1) ;
     }
@@ -38,52 +37,53 @@ result_t* subprocess(char* program,char* str){
     pid_t child = fork();
 
     if(child > 0){
-        close(pipes2[1]);
-        close(pipes3[1]);
+        result_t* temp = (result_t*)malloc(sizeof(result_t));
+        write(pipe_stdin[1],str,strlen(str));
 
-        // dprintf(pipes[1],"%s",str);
-        write(pipes[1],str,sizeof(str));
-        close(pipes[1]);
+        close(pipe_stdout[1]);
+        close(pipe_stderr[1]);
+        close(pipe_stdin[1]);
 
         int status;
         wait(&status);
         temp->return_code = status;
 
         if(temp->return_code == 0){
-            temp->outcome = (char*)malloc(sizeof(char)*strlen(PASS)+1);
+            temp->outcome = (char*)malloc(sizeof(char)*(strlen(PASS)+1));
             strcpy(temp->outcome,PASS);
         }else if(temp->return_code < 0){
-            temp->outcome = (char*)malloc(sizeof(char)*strlen(FAIL)+1);
-            strcpy(temp->outcome,FAIL);
+            temp->outcome = (char*)malloc(sizeof(char)*(strlen(FAIL)+1));
+            strcpy(temp->outcome,FAIL); 
         }else{
-            temp->outcome = (char*)malloc(sizeof(char)*strlen(UNRESOLVED)+1);
+            temp->outcome = (char*)malloc(sizeof(char)*(strlen(UNRESOLVED)+1));
             strcpy(temp->outcome,UNRESOLVED);
         }
 
-        temp->output = (char*)malloc(sizeof(char)*1024);
-        if(read(pipes2[0],temp->output,1024) < 0){
+        temp->output = (char*)malloc(sizeof(char)*1024);    //TODO append
+        if(read(pipe_stdout[0],temp->output,length) < 0){
             printf("read failed\n");
             exit(0);
         }
-
-        temp->error = (char*)malloc(sizeof(char)*1024);
-        if(read(pipes3[0],temp->error,1024) < 0){
+        temp->output[length] = '\0';
+        temp->error = (char*)malloc(sizeof(char)*1024);     //TODO append
+        if(read(pipe_stderr[0],temp->error,1024) < 0){
             printf("read failed\n");
             exit(0);
         }
+        return temp;
     }else if(child == 0){
-        dup2(pipes[0],STDIN_FILENO);
-        dup2(pipes2[1],STDOUT_FILENO);
-        dup2(pipes3[1],STDERR_FILENO);
+        
+        dup2(pipe_stdin[0],STDIN_FILENO);
+        dup2(pipe_stdout[1],STDOUT_FILENO);
+        dup2(pipe_stderr[1],STDERR_FILENO);
 
-        close(pipes[0]);
-        close(pipes[1]);
-        close(pipes2[0]);
-        close(pipes3[0]);
+        close(pipe_stdin[0]);
+        close(pipe_stdin[1]);
+        close(pipe_stdout[0]);
+        close(pipe_stderr[0]);
 
         execlp(program,program,NULL);
     }
-    return temp;
 }
 
 void delete_result(result_t* tmp){
