e6a532447302341cb9af7fe4d32eed092a91968d
Hwangwoosam <sam1783@naver.com>
2021-08-11 13:05:33 +0900

    PrintRunner

---
+++ CH2/Runner_class/PrintRunner/print_runner.c
@@ -0,0 +1,27 @@
+#include "print_runner.h"
+
+void PrintRuner_init(Runner * print_runner){
+    print_runner->input = 0x0;
+    print_runner->outcome = 0x0;
+    print_runner->free_inp = free_input;
+    print_runner->run = PrintRunner_run;
+}
+
+int PrintRunner_run(Runner* print_runner,char* input, int inp_size){
+    int ret = run(print_runner,input,inp_size);
+    for(int i = 0; i < print_runner->inp_size; i++){
+        printf("%d %c\n",i,print_runner->input[i]);
+    }
+    printf("\n");
+
+    return ret;    
+}
+
+int main(){
+    Runner print_runner;
+    PrintRuner_init(&print_runner);
+    char input[] = "\0he\0llo";
+
+    print_runner.run(&print_runner,input,sizeof(input));
+    print_runner.free_inp(print_runner);
+}
---
+++ CH2/Runner_class/PrintRunner/print_runner.h
@@ -0,0 +1,5 @@
+#include "../runner.h"
+
+void PrintRunner_init (Runner * runner);
+
+int PrintRunner_run();
