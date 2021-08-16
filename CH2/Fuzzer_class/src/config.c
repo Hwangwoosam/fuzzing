9c9ce0a7caf8757d5ad0ebd3d8eb0dcd9c3927bc
Hwangwoosam <sam1783@naver.com>
2021-08-16 10:28:21 +0900

    add default oracle

--- CH2/Fuzzer_class/src/config.c
+++ CH2/Fuzzer_class/src/config.c
@@ -3,16 +3,20 @@
 #include <string.h>
 #include "../include/config.h"
 
+int default_oracle(char* dir_name,int trial,int return_code){
+    return return_code;
+}
+
 void init_config(test_config_t * config){
     config->inp_arg.f_min_len = 10;
     config->inp_arg.f_max_len = 100;
     config->inp_arg.f_char_start = 32;
     config->inp_arg.f_char_range = 32;
 
-    config->oracle = NULL;          //set the default oracle
+    config->oracle = default_oracle;          //set the default oracle
     config->trial = 10;
 
-    config->run_arg.timeout = 5;
+    config->run_arg.timeout = 2;
     config->run_arg.cmd_args = NULL;
     config->run_arg.args_num = 0;
 
