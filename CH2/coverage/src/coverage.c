68fd70fe6a44c533fbdd40524b47f8c5d06e4a61
Hwangwoosam <sam1783@naver.com>
2021-08-20 12:39:58 +0900

    modify coverage.c

--- CH2/coverage/src/coverage.c
+++ CH2/coverage/src/coverage.c
@@ -1,5 +1,19 @@
 #include "../include/coverage.h"
+#include "../../Fuzzer_class/include/config.h"
 
+void coverage_init(run_arg_t run_config,coverage_set_t* cover_set,int trial){
+    coverage_compile(run_config.src_path,run_config.binary_path);
+    cover_set->src_file_name = get_filename(run_config.src_path);
+    coverage_gcov(cover_set->src_file_name);
+    char gcove_file[64];
+    sprintf(gcove_file,"%s.gcov",cover_set->src_file_name);
+    cover_set->code_size = get_code_line(gcove_file);
+
+    cover_set->line_check = (int*)malloc(sizeof(int)*cover_set->code_size[1]);
+    cover_set->execute_check = (int*)malloc(sizeof(int)*trial);
+    memset(cover_set->execute_check,0,sizeof(char)*trial);
+    memset(cover_set->line_check,0,sizeof(char)*cover_set->code_size[1]);
+}
 
 int coverage_compile(char *test_file,char* exc_file){
     int pid = fork();
@@ -11,7 +25,6 @@ int coverage_compile(char *test_file,char* exc_file){
         exit(1);
     }else if(pid > 0){
         wait(&status);
-        printf("compile complete\n");
     }else{
         perror("compile failed\n");
         exit(1);
@@ -54,7 +67,6 @@ int coverage_gcov(char* test_file){
         exit(1);
     }else if(pid > 0){
         wait(&status);
-        printf("gcov complete\n");
     }else{
         perror("compile failed\n");
         exit(1);
@@ -76,33 +88,7 @@ char* get_filename(char* path){
     return cpy_file_name;
 }
 
-void make_gcov(int argc,char* argv[]){
-    char* src_filename = get_filename(argv[1]);
-    
-    int file_length = strlen(src_filename);
-    char* exc_file = (char*)malloc(sizeof(char)*file_length);
-    memcpy(exc_file,src_filename,file_length-2);
-    char** args;
-    if(argc == 2){
-        args = (char**)malloc(sizeof(char*)*argc);
-        args[0] = exc_file;
-        args[1] = NULL;
-    }else if(argc > 2){
-        args = (char**)malloc(sizeof(char*)*argc);
-        args[0] = exc_file;
-        for(int i = 0; i < argc-2 ; i++){
-            args[i+1] = argv[i+2];
-        }
-    }
-    args[argc-1] = NULL; 
-    coverage_compile(argv[1],args[0]);
-    coverage_excute(args[0]); 
-    coverage_gcov(src_filename);
-    free(args);
-    free(src_filename);
-}
-
-void read_gcov(char* path){
+void read_gcov(char* path,int* result,int* list){
     char* src_file = get_filename(path);
     FILE* fp;
     char* line = (char*)malloc(sizeof(char)*256) ;
@@ -115,17 +101,60 @@ void read_gcov(char* path){
         perror("file open failed\n");
         exit(1);
     }
+    char*ptr;
+    int num=0;
     while((s = getline(&line,&len,fp)) != -1){
-        char*ptr = strtok(line,":");
+        ptr = strtok(line,":");
         int execution = atoi(ptr);
         if(execution > 0){
-            printf("execute: %d : ",atoi(ptr));
             ptr = strtok(NULL,":");
             int line_num = atoi(ptr);
-            printf("%d\n",atoi(ptr));
+            if(list[line_num] == 0){
+                list[line_num]++;
+            }
+            num++;
         }
     }
+    *result = num;
     fclose(fp);
     free(line);
     free(src_file);
+}
+
+int* get_code_line(char* path){
+    FILE* fp = fopen(path,"rb");
+    int num = 0;
+    int excute = 0;
+    char* line = NULL;
+    size_t len = 0;
+    int* size = (int*)malloc(sizeof(int)*2);
+    while(!feof(fp) && getline(&line,&len,fp) != 1){
+        if(strstr(line,"####")){
+            excute++;
+        }
+        num++;
+    }
+    size[0] = excute;
+    size[1] = num;
+    return size;
+}
+
+
+void reset_gcda(char* path){
+    int path_length = strlen(path);
+    char* gcda_path = (char*)malloc(sizeof(char)*(path_length+3));
+    sprintf(gcda_path,"./%s.gcda",path);
+    if(access(gcda_path,R_OK) == 0){
+        if(remove(gcda_path) != 0){
+            perror("remove error\n");
+        }
+    }
+}
+
+void coverage_free(coverage_set_t* cover_set){
+    free(cover_set->code_size);
+    free(cover_set->execute_check);
+    free(cover_set->line_check);
+    free(cover_set->src_file_name);
+    free(cover_set);
 }
