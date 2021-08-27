b46b863114785912767f42b20f03154c3d0972cd
Hwangwoosam <sam1783@naver.com>
2021-08-27 13:48:26 +0900

    update comment

---
+++ CH2/Fuzzer/include/config.h
@@ -0,0 +1,37 @@
+#ifndef CONFIG
+#define CONFIG
+#define PATH_MAX 4096
+
+//contains information about input
+
+typedef struct input_arg{
+    int f_min_len;      // minimum length about random string
+    int f_max_len;      // max length about random string
+    int f_char_start;   // start point and range in character
+    int f_char_range;
+}input_arg_t;
+
+// contains information need to run fuzzer
+typedef struct run_arg{
+    char* binary_path; 
+    char* src_path;
+    char** cmd_args;    // list of argument
+    int args_num;       // number of argument
+    int timeout;        
+    int fuzz_type;      // 0 = stdin , 1 = argument 
+}run_arg_t;
+
+
+
+typedef struct test_config{
+    input_arg_t inp_arg;
+    run_arg_t run_arg;
+
+    int trial;
+    int (* oracle)(char* dir_name,int trial,int ret_code);
+    
+}test_config_t;
+
+void init_config(test_config_t *);
+
+#endif
---
+++ CH2/Fuzzer/include/create_inp.h
@@ -0,0 +1,8 @@
+#ifndef FUZZ_INPUT
+#define FUZZ_INPUT
+
+#include "config.h"
+
+void create_inp(char* random_inp,int* inp_size, input_arg_t inp_config);
+
+#endif
