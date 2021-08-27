42496e65d95559ac4c76d527d836ee3d97daa28c
Hwangwoosam <sam1783@naver.com>
2021-08-27 17:04:58 +0900

    modify create_inp.c and fuzzer.c

--- CH2/Fuzzer/src/create_inp.c
+++ CH2/Fuzzer/src/create_inp.c
@@ -1,15 +1,16 @@
-#include "../include/create_inp.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "../include/create_inp.h"
 // create random string input
-void create_inp(char* random_inp,int * inp_size,input_arg_t inp_config){
+void create_inp(char* random_inp,int * inp_size,config_t test_config){
     
-    int string_length = rand()%(inp_config.f_max_len+1);
+    int string_length = rand()%(test_config.inp_arg.f_max_len+1);
+    printf("%d size\n",test_config.inp_arg.f_max_len);
     *inp_size = string_length; 
     for(int i = 0; i < string_length; i++){
-        char tmp = rand()%(inp_config.f_char_range + 1) + inp_config.f_char_start;
+        char tmp = rand()%(test_config.inp_arg.f_char_range + 1) + test_config.inp_arg.f_char_start;
         random_inp[i] = tmp;
     }
     random_inp[string_length] = '\0';
