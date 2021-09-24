2a6ae82f3ae2654b8b2821190b26f6670383341f
Hwangwoosam <sam1783@naver.com>
2021-09-24 14:18:41 +0900

    coverage refactoring

--- CH3/mutation_fuzzer/include/coverage.h
+++ CH3/mutation_fuzzer/include/coverage.h
@@ -6,29 +6,26 @@
 #include <unistd.h>
 #include "config.h"
 #include <sys/wait.h>
-typedef struct branch_set{
-    char* input;
-    int branch_num;
-}branch_set_t;
 
 typedef struct coverage_set{
-    int* code_size;
-    char* src_file_name;
-    int* line_check;     
-    int* execute_check;
-    int* branch_check;
-    int* e_branch_check;
-    branch_set_t* branch_list;
-    int list_length;
+    int** code_size;
+    char** src_file_name;
+    int src_file_num;
+    int** line_check;     // total line
+    int** execute_check;  // excute line
+    int** branch_check;
+    int** e_branch_check;
+    char** branch_set;
+    int* branch_length;
 }coverage_set_t;
 
-void coverage_init(run_arg_t run_config,coverage_set_t* cover_set,int trial);
-int coverage_compile(char *test_file,char* exc_file);
+void coverage_init(config_t* config,coverage_set_t* cover_set);
 int coverage_excute(char* exc_name);
-int coverage_gcov(char* test_file);
-char* get_filename(char* path);
+int coverage_gcov(char* test_file,char* src_dir);
+int get_filename(char* path,char* target);
+char* remove_type(char* path);
 void make_gcov(int argc,char* argv[]);
-void read_gcov(char* path,int* line_result,int* branch_result,int* list,int* total_branch,branch_set_t* branch_list,int* list_length,char* random, int random_size);
+void read_gcov(coverage_set_t* cover_set,config_t* config,char* random, int random_size,int num,int trial);
 int* get_code_line(char* path);
-void reset_gcda(char* path);
-void coverage_free(coverage_set_t* cover_set);
+void reset_gcda(config_t config,int src_num);
+void coverage_free(coverage_set_t* cover_set,int num);
