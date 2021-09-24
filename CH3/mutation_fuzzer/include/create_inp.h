3ffcb864126551d0bf728a25f112b521bfe5c450
Hwangwoosam <sam1783@naver.com>
2021-09-24 14:20:13 +0900

    create refactoring

--- CH3/mutation_fuzzer/include/create_inp.h
+++ CH3/mutation_fuzzer/include/create_inp.h
@@ -4,8 +4,14 @@
 #include "./config.h"
 
 void create_inp(char* random_inp,int* inp_size, input_arg_t inp_config);
-int delete_random(char* str,char* target,int str_size);
-int insert_random(char* str,char* target,int str_size);
+int delete_random(char* str,char* target,int str_size,int byte);
+int insert_random(char* str,char* target,int str_size,int byte);
+int random_value(char* str, char* target, int str_size, int byte);
 int flip_random(char* str,char* target,int str_size);
+int byte_flip(char* str, char* target ,int str_size,int byte);
+int simple_arithmatic_insert(char* str,char* target,int str_size,int byte);
+int simple_arithmatic_change(char* str,char* target,int str_size,int byte);
+int known_integers_insert(char* str, char* target ,int str_size,int byte);
+int known_integers_change(char* str, char* target ,int str_size,int byte);
 int mutate(char* str,char* target,int str_size);
 #endif
