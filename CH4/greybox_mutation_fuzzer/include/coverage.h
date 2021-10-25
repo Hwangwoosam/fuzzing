5e0612b1c26407b92cfed968749478df2d83bf58
Hwangwoosam <sam1783@naver.com>
2021-10-25 18:25:03 +0900

    boosted greybox

--- CH4/greybox_mutation_fuzzer/include/coverage.h
+++ CH4/greybox_mutation_fuzzer/include/coverage.h
@@ -5,23 +5,18 @@
 #include <string.h>
 #include <unistd.h>
 #include "config.h"
+#include "sched.h"
 #include <sys/wait.h>
-typedef struct branch_set{
-    char** input;
-    int* input_length;
-    int branch_length;
-    int* line_num;
-}branch_set_t;
 
 typedef struct coverage_set{
     int* code_size[NUM_OF_MAX];
     int* line_check[NUM_OF_MAX];     // total line
-    int* e_line_check[NUM_OF_MAX];  // excute line
+    int* e_line_check[NUM_OF_MAX];   // excute line
     int* branch_check[NUM_OF_MAX];
     int* e_branch_check[NUM_OF_MAX];
     int* total_excute_line;
     int* total_excute_branch;
-    branch_set_t branch_set;
+    char hashtable[65537];
 }coverage_set_t;
 
 void coverage_init(config_t* config,run_arg_t run_arg,coverage_set_t* cover_set);
@@ -30,7 +25,7 @@ int coverage_gcov(char* test_file,char* src_dir);
 int get_filename(char* path,char* target);
 char* remove_type(char* path);
 void make_gcov(int argc,char* argv[]);
-void read_gcov(coverage_set_t* cover_set,run_arg_t* run_arg,char* random, int random_size,int src_num,int trial);
+void read_gcov(coverage_set_t* cover_set,run_arg_t* run_arg,sched_info_t* sched_info,char* random, int random_size,int src_num,int trial);
 int* get_code_line(char* path);
 void reset_gcda(run_arg_t run_arg,int src_num);
 void coverage_free(coverage_set_t* cover_set,int num);
